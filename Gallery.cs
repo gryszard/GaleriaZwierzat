93ab254be2e036db1e0a9274ae9669ad4ab42aea
Ryszard Gromek <rysiupysiu234@gmail.com>
2024-11-20 21:47:44 +0100

    Add classes

---
+++ Gallery.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaleriaZwierzat;
+
+internal class Gallery
+{
+    private List<Picture> Pictures;
+
+    public List<Picture> GetPictures()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeletePicture(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddPicture(Picture picture)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DisplayPictures()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ PicturesManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaleriaZwierzat;
+
+internal class PicturesManager
+{
+    private Picture pictureToSave;
+
+    public void AddTitle(string title)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SavePictureToDb(Picture picture)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeletePictureFromDb(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Picture> GetPicturesFromDb()
+    {
+        throw new NotImplementedException();
+    }
+}
